b1b8a347cfde63ec15a4142348fc6974277cfa58
steven4244 <steven4244@gmail.com>
2024-12-05 07:53:49 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -52,10 +52,19 @@ def submit_ticket():
     except Exception as e:
         return jsonify({'success': False, 'message': f'Error: {str(e)}'}), 500
 
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    "mysql+pymysql://doadmin:AVNS_Ra0O73y2zLLf4mstnmk@newturnteamocean-do-user-18369686-0.m.db.ondigitalocean.com:25060/defaultdb"
-)
 
+# Read the environment variables securely
+db_user = os.getenv('DB_USER')  # Your database username
+db_password = os.getenv('DB_PASSWORD')  # Your database password
+db_host = os.getenv('DB_HOST')  # Your database host
+db_port = os.getenv('DB_PORT')  # Your database port
+db_name = os.getenv('DB_NAME')  # Your database name
+db_sslmode = os.getenv('DB_SSLMODE')  # SSL Mode
+
+# Construct the SQLAlchemy database URI using the environment variables
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}?ssl-mode={db_sslmode}"
+)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
